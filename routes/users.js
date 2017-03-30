5fc66072544a81305ae4053a5efd910f0a2ab38e
ixistic <teeravipark@gmail.com>
2017-03-30 16:54:34 +0700

    Try Restful user

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/user', function(req, res, next) {
   res.send('respond with a resource');
 });
+router.post('/user', function (req, res) {
+  res.send('Got a POST request')
+});
+router.put('/user', function (req, res) {
+  res.send('Got a PUT request at /user')
+});
+router.delete('/user', function (req, res) {
+  res.send('Got a DELETE request at /user')
+});
+
 
 module.exports = router;
