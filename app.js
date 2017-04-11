3a635649c0053c2a89fda06e5672f34f6d447922
ixistic <teeravipark@gmail.com>
2017-04-11 14:12:15 +0700

    Implement inteval recieve queue request

--- app.js
+++ app.js
@@ -4,10 +4,56 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+
+var models = require('./models');
 var auth = require("./authentication/auth.js")();
+var users = require("./authentication/users.js");
+var cfg = require("./authentication/config.js");
+var request = require('request');
+var jwt = require("jwt-simple");
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+
+function pollingAuth() {
+  request({
+    method: 'GET',
+    url: 'https://aitsgqueues.mrteera.com/messages/erp_response',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }, function (error, auth_resp, resp_body) {
+    if(JSON.parse(resp_body).message){
+      console.log('Status:', auth_resp.statusCode);
+      console.log('Headers:', JSON.stringify(auth_resp.headers));
+      console.log('Response:', resp_body);
+      var auth_body = JSON.parse(JSON.parse(resp_body).message);
+      var gid = auth_body.gid;
+      var g_name = auth_body.name;
+      if (auth_body.auth == "ok") {
+        var payload = {
+          gid: gid
+        };
+        var token = jwt.encode(payload, cfg.jwtSecret);
+        models.Guard
+          .findOrCreate({where: {token: token}, defaults: {gid: gid, name: g_name, token: token}})
+          .spread(function(guard, created) {
+            console.log(guard.get({
+              plain: true
+            }));
+            console.log("ok 200 send token");
+            //TODO FCM send message (success with token)
+            //TODO delete queue message
+          })
+      } else {
+        //TODO FCM send message (fail)
+        //TODO delete queue message
+        console.log("fail 401");
+      }
+    }
+  });
+}
+setInterval(pollingAuth, 3000);
 
 var app = express();
 
--- routes/users.js
+++ routes/users.js
@@ -13,42 +13,9 @@ router.get("/user", auth.authenticate(), function (req, res) {
 });
 
 router.post("/token", function (req, res) {
-  var auth_status = false;
-  request({
-    method: 'GET',
-    url: 'https://aitsgqueues.mrteera.com/messages/erp_response',
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  }, function (error, auth_resp, resp_body) {
-    console.log('Status:', auth_resp.statusCode);
-    console.log('Headers:', JSON.stringify(auth_resp.headers));
-    console.log('Response:', resp_body);
-    var auth_body = JSON.parse(JSON.parse(resp_body).message);
-    var gid = auth_body.gid;
-    var g_name = auth_body.name;
-    if (auth_body.auth == "ok") {
-      auth_status = true;
-      console.log(auth_status);
-      var payload = {
-        gid: gid
-      };
-      var token = jwt.encode(payload, cfg.jwtSecret);
-
-      models.Guard
-        .findOrCreate({where: {token: token}, defaults: {gid: gid, name: g_name, token: token}})
-        .spread(function(guard, created) {
-          console.log(guard.get({
-            plain: true
-          }));
-          console.log(created);
-          res.json({
-            token: token
-          })
-        })
-    } else {
-      res.sendStatus(401);
-    }
+  //todo - post message to queue
+  res.json({
+    message: "loggin in"
   });
 });
 
