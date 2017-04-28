11b081fe8825ddeaeec78b7c5f5246b82374e460
mrteera <inbox@mrteera.com>
2017-04-28 23:58:18 +0700

    Update backend simulation script to send data back with device token

--- scripts/erp_sim.py
+++ scripts/erp_sim.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 
 import requests
+# import simplejson as json
+import json
 
 def erp_process_message_from_req_queue():
     resp = requests.get('https://aitsgqueues.mrteera.com/messages/erp_request')
@@ -9,18 +11,29 @@ def erp_process_message_from_req_queue():
 
     if ('{}' == str(message_from_req)):
         print('No message in the queue: ' + str(message_from_req))
+        return None
     else:
         print('Found a message in the queue: ' + str(message_from_req))
         message_id = message_from_req['id']
-        resp = requests.delete('https://aitsgqueues.mrteera.com/messages/erp_request/{}'.format(message_id))
-        print('status: ' + str(resp.status_code))
+        return message_from_req['message'], message_id
 
-def erp_put_message_back_into_resp_queue():
+def erp_put_message_back_into_resp_queue(message, message_id):
+    if message == None:
+        return None
+    message = json.loads(message.encode('utf-8').replace("'", '"'))
+    resp_msg = "{\r\n\t\"gid\": \"%s\"" % message['username']
+    resp_msg += ",\r\n\t\"name\": \"noname\""
+    resp_msg += ",\r\n\t\"auth\": \"true\""
+    resp_msg += ",\r\n\t\"device_token\": \"%s\"\r\n}" % message['device-token']
     payload = { "qname": "erp_response",
-                "message": "{\r\n\t\"gid\": \"g11543\",\r\n\t\"name\": \"Somchai\",\r\n\t\"auth\": \"true\"\r\n}"
+		"message": resp_msg
     }
     resp = requests.post('https://aitsgqueues.mrteera.com/messages/erp_response', payload)
+    print("erp_response status coode: " + str(resp.status_code))
+    if resp.status_code == 200:
+        resp = requests.delete('https://aitsgqueues.mrteera.com/messages/erp_request/{}'.format(message_id))
+        print('delete message status: ' + str(resp.status_code))
 
 if __name__ == '__main__':
-    erp_process_message_from_req_queue()
-    erp_put_message_back_into_resp_queue()
+    message, message_id = erp_process_message_from_req_queue()
+    erp_put_message_back_into_resp_queue(message, message_id)
