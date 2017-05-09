989099dac6628c490c1589191eaa83df4e44dd71
ixistic <teeravipark@gmail.com>
2017-05-09 19:27:49 +0700

    Fix permission

--- routes/users.js
+++ routes/users.js
@@ -7,25 +7,13 @@ var cfg = require("../authentication/config.js");
 var jwt = require("jwt-simple");
 var request = require('request');
 var jsesc = require('jsesc');
-var cors = require('cors')
-
-var whitelist = ['https://aitsgmanager.mrteera.com', 'http://aitsgmanager.mrteera.com']
-var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
 
 router.get("/user", auth.authenticate(), function (req, res) {
   res.json(users[0]);
   //res.json(users[req.user.id]);
 });
 
-router.post("/token", cors(corsOptions), function (req, res, next) {
+router.post("/token", cors(corsOptions), function (req, res) {
   var req_message = jsesc(req.body);
   payload = {
     "qname": "erp_request",
